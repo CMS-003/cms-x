28abe6648dca1c55d76bb95720fdd2f98b3ee89a
阮家友 <1439120442@qq.com>
2025-04-24 16:58:13 +0800

    滚动折叠 Filter

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -11,13 +11,14 @@ export default function PageList({ items, onRefresh, loadMore, multi = false, ha
         "--padding-left": 0,
         "--padding-right": 0,
         "--border-inner": "none",
-        "--adm-color-background": "transparent"
+        "--adm-color-background": "transparent",
+        padding: '0 5px'
       }}>
         {items.map((item, i) => (
           <List.Item key={i}>
             {
               multi
-                ? <FullWidth style={{ gap: 8, marginTop: 8 }}>
+                ? <FullWidth style={{ gap: 5, marginBottom: 5 }}>
                   {item.map(v => <ResourceItem key={v._id} item={v} type="half" />)}
                 </FullWidth>
                 : <ResourceItem key={item._id} item={item} />
--- src/groups/Filter/index.js
+++ src/groups/Filter/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
-import { useCallback } from 'react';
+import { Fragment, useCallback, useRef } from 'react';
 import { useEffectOnce } from 'react-use';
-import { toJS } from 'mobx';
+import { action, toJS } from 'mobx';
 import { Observer, useLocalObservable } from 'mobx-react-lite';
 import styled from 'styled-components';
 import { FullHeight, FullHeightAuto, FullHeightFix } from '../../components/style'
@@ -14,11 +14,22 @@ const Wrap = styled.div`
   align-items: center;
   align-content: center;
   overflow-x: auto;
-  padding: 2px 0;
+  padding-top: 5px;
   width: 100%;
   &::-webkit-scrollbar {
     display: none;
   }
+`
+const ShortWrap = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #eee;
+  z-index: 2;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 0;
 `
 const Tag = styled.span`
   display: inline-block;
@@ -34,6 +45,10 @@ export default function CFilter({ self }) {
     resources: [],
     page: 1,
     hasMore: true,
+    // 顶部高度
+    headHeight: 'auto',
+    // 顶部状态: default,fixShort,fixHead
+    headStatus: 'default',
     getQuery() {
       const ids = [...self.queries];
       self.children.forEach(child => {
@@ -47,7 +62,16 @@ export default function CFilter({ self }) {
     },
     setResources(resources) {
       local.resources = resources;
-    }
+    },
+    setHeadHeight: action((height) => {
+      local.headHeight = height;
+    }),
+    setHeadStatus: action((status) => {
+      local.headStatus = status;
+    }),
+    setHasMore: action((has) => {
+      local.hasMore = has;
+    }),
   }));
   const getData = useCallback(async () => {
     const qid = local.getQuery()
@@ -56,7 +80,7 @@ export default function CFilter({ self }) {
       const resp = await apis.getResourceList({ qid, page: local.page, size: 20 });
       if (resp.code === 0) {
         local.page === 1 ? local.setResources(resp.data.items) : local.setResources(local.resources.concat(resp.data.items))
-        local.hasMore = resp.data.items.length > 0;
+        local.setHasMore(resp.data.items.length > 0)
       }
     } catch (e) {
       console.log(e);
@@ -76,23 +100,45 @@ export default function CFilter({ self }) {
   })
   return <Observer>
     {() => (
-      <FullHeight style={toJS(self.style)}>
-        <FullHeightFix style={{ flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
-          {self.children.map(child => (
-            <Wrap key={child._id}>
-              {child.children.map(sun => (
-                <Tag key={sun._id} style={sun.attrs.selected ? { color: 'white', backgroundColor: '#3498db' } : { color: '#bbb', backgroundColor: 'white' }} onClick={() => {
-                  child.children.forEach(v => {
-                    v.attrs.selected = v._id === sun._id
-                  });
-                  local.page = 1;
-                  getData();
-                }}>{sun.title}</Tag>
+      <div style={{ height: '100%', position: 'relative' }}>
+        <div style={{ height: '100%', overflow: 'auto', ...toJS(self.style) }} onScroll={e => {
+          if (local.headHeight !== 'auto') {
+            local.setHeadStatus(e.currentTarget.scrollTop > local.headHeight ? 'fixShort' : 'default')
+          }
+          if (local.headStatus !== 'fixShort' && local.headHeight !== 'auto' && e.currentTarget.scrollTop > local.headHeight) {
+            local.setHeadStatus('fixShort');
+          }
+        }}>
+          <div style={{ height: local.headHeight }}>
+            <FullHeightFix style={{ position: local.headStatus === 'fixHead' ? 'absolute' : 'static', top: 0, left: 0, flexDirection: 'column', alignItems: 'flex-start', width: '100%', zIndex: 2, padding: '0 5px 5px', boxSizing: 'border-box', backgroundColor: '#eee' }} ref={ref => {
+              if (ref && local.headHeight === 'auto') {
+                local.setHeadHeight(ref.offsetHeight)
+              }
+            }}>
+              {self.children.map(child => (
+                <Wrap key={child._id}>
+                  {child.children.map(sun => (
+                    <Tag key={sun._id} style={sun.attrs.selected ? { color: 'white', backgroundColor: '#3498db' } : { color: '#bbb', backgroundColor: 'white' }} onClick={() => {
+                      child.children.forEach(v => {
+                        v.attrs.selected = v._id === sun._id
+                      });
+                      local.page = 1;
+                      getData();
+                    }}>{sun.title}</Tag>
+                  ))}
+                </Wrap>
               ))}
-            </Wrap>
-          ))}
-        </FullHeightFix>
-        <FullHeightAuto>
+            </FullHeightFix>
+            <ShortWrap style={{ display: local.headStatus === 'fixShort' ? 'flex' : 'none' }} onClick={() => local.setHeadStatus('fixHead')}>
+              {self.children.map(child => (
+                <Fragment key={child._id}>
+                  {child.children.map(sun => (
+                    sun.attrs.selected ? <Tag key={sun._id} style={{ color: 'white', backgroundColor: '#3498db' }}>{sun.title}</Tag> : null
+                  ))}
+                </Fragment>
+              ))}
+            </ShortWrap>
+          </div>
           <List
             multi={true}
             items={_.chunk(local.resources, 2)}
@@ -103,8 +149,8 @@ export default function CFilter({ self }) {
             loadMore={getMore}
             hasMore={local.hasMore}
           />
-        </FullHeightAuto>
-      </FullHeight>
+        </div>
+      </div>
     )}
   </Observer>
 }
