b0bd9eec8b691784545a6770f2695a194937ea31
sophia <1439120442@qq.com>
2026-04-10 15:41:20 +0800

    兼容pwa和app环境

--- src/components/SafeArea/index.js
+++ src/components/SafeArea/index.js
@@ -4,8 +4,8 @@ import { FullHeight, FullHeightFix } from "@/components";
 
 export default function SafeArea({
   height = '100%',
-  top = 'var(--safe-padding-top)',
-  bot = 'var(--safe-padding-bottom)',
+  top = 'max(var(--safe-padding-top), env(safe-area-inset-top)',
+  bot = 'max(var(--safe-padding-bottom), env(safe-area-inset-bottom)',
   topBGC = 'transparent',
   botBGC = 'transparent',
   children,
